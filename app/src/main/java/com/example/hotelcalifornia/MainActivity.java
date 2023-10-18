4b989be1843c992a3f21aba5118c642162a0f366
Shirley Frassa <slfrassa@gmail.com>
2023-10-17 23:59:05 -0300

    FEATURE: Se crea Db con la API de AndroidStudio.

--- app/src/main/java/com/example/hotelcalifornia/MainActivity.java
+++ app/src/main/java/com/example/hotelcalifornia/MainActivity.java
@@ -4,17 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.hotelcaliforniaDatos.HotelSQLiteHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-
     EditText emailLogin, passwordLogin;
-     Button inicio;
+    Button inicio;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,19 +27,20 @@ public class MainActivity extends AppCompatActivity {
         passwordLogin=findViewById(R.id.passwordLogin);
         inicio=findViewById(R.id.inicio);
 
-
+        // Creamos o hacemos conexión a la DB
+        HotelSQLiteHelper hotelSqlHelper = new HotelSQLiteHelper(this, "DbHotelCalifornia", null, 1);
+        SQLiteDatabase db = hotelSqlHelper.getReadableDatabase();
+        // continuar con el código para obtener datos de la db y poder loguearse.
     }
 
     public void iraregistro(View view){
         Intent intent = new Intent(this, Registro.class);
         startActivity(intent);
     }
+
     public void home(View view){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
-
-
-
 }
 
